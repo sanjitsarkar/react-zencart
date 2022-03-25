e4639d84bd6739001b8ef60784fe2785c1f3bb16
Sanjit <ssanjit856@gmail.com>
2022-03-25 23:44:50 +0530

    set up project structure for e-commerce project

---
+++ src/backend/db/categories.js
@@ -0,0 +1,51 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const categories = [
+  {
+    _id: uuid(),
+    categoryName: "Headphones",
+    description:
+      "A video game console is an electronic device that outputs a video signal or image to display a video game that can be played with a game controller.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Keyboards",
+    description:
+      "A game is a structured form of play, usually undertaken for entertainment or fun.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Speakers",
+    description:
+      "A video game accessory is a distinct piece of hardware that is required to use a video game console, or one that enriches the video game's play experience.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Studio Mic",
+    description:
+      "A video game accessory is a distinct piece of hardware that is required to use a video game console, or one that enriches the video game's play experience.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "DJ Kits",
+    description:
+      "A video game accessory is a distinct piece of hardware that is required to use a video game console, or one that enriches the video game's play experience.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Midi Keyboard",
+    description:
+      "A video game accessory is a distinct piece of hardware that is required to use a video game console, or one that enriches the video game's play experience.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Guitars",
+    description:
+      "A video game accessory is a distinct piece of hardware that is required to use a video game console, or one that enriches the video game's play experience.",
+  },
+];
