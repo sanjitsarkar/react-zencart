7fe9a32b4fb7f1a502b79d1fea12a37d74bda03c
Sanjit <ssanjit856@gmail.com>
2022-03-28 13:40:04 +0530

    fixed filter product functionality

--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -1,30 +1,98 @@
 import axios from "axios";
 import React, { useEffect, useState, createContext, useContext } from "react";
 import { useProduct } from "./ProductsContext";
+const initialState = {
+  price: 5000,
+  categories: [],
+  brands: [],
+  rating: 4,
+  sortBy: "price",
+};
 const FiltersContext = createContext();
 const FiltersProvider = ({ children }) => {
-  const [products, setProducts] = useProduct();
-  const [filters, setFilters] = useState({
-    price: 8000,
-    categories: [],
-    brands: [],
-    rating: 4,
-  });
+  const [products, setProducts, searchProducts, fetchProducts] = useProduct();
+  const [filters, setFilters] = useState(initialState);
 
-  useEffect(() => {
-    console.log("filter", filters);
-    const _products = products.data.filter(
-      (product) =>
-        filters.brands.includes(product.brand) && filters.price <= product.price
-    );
+  const resetFilters = () => {
+    searchProducts();
+  };
+  const handleCategories = (e) => {
+    console.log("VALUE", e.target.value);
+    !filters.categories.includes(e.target.value)
+      ? setFilters((_filters) => {
+          return {
+            ..._filters,
+            categories: [...filters.categories, e.target.value],
+          };
+        })
+      : setFilters((_filters) => {
+          return {
+            ..._filters,
+            categories: filters.categories.filter(
+              (category) => category !== e.target.value
+            ),
+          };
+        });
+  };
+  const handleBrands = (e) => {
+    !filters.brands.includes(e.target.value)
+      ? setFilters((_filters) => {
+          return {
+            ..._filters,
+            brands: [...filters.brands, e.target.value],
+          };
+        })
+      : setFilters((_filters) => {
+          return {
+            ..._filters,
+            brands: filters.brands.filter(
+              (brands) => brands !== e.target.value
+            ),
+          };
+        });
+  };
+  useEffect(async () => {
+    const fetchedProducts = await fetchProducts();
+    const _products = fetchedProducts.data.filter((product) => {
+      if (filters.categories.length > 0) {
+        if (!filters.categories.includes(product.category)) {
+          return false;
+        }
+      }
+      if (filters.brands.length > 0) {
+        if (!filters.brands.includes(product.brand)) {
+          return false;
+        }
+      }
+      if (filters.price) {
+        if (product.price > filters.price) {
+          return false;
+        }
+      }
+      if (filters.rating) {
+        if (product.rating < filters.rating) {
+          return false;
+        }
+      }
+      return true;
+    });
     setProducts({
       loading: false,
       data: _products,
       error: "",
     });
+    console.log("pro", _products);
   }, [filters]);
   return (
-    <FiltersContext.Provider value={[filters, setFilters]}>
+    <FiltersContext.Provider
+      value={[
+        filters,
+        setFilters,
+        handleCategories,
+        handleBrands,
+        resetFilters,
+      ]}
+    >
       {children}
     </FiltersContext.Provider>
   );
