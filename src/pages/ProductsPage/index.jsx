6a59d5bcdd84635b357907ca43066c88d12595de
Sanjit <ssanjit856@gmail.com>
2022-03-29 20:18:13 +0530

    refactored and removed unused codes

--- src/pages/ProductsPage/index.jsx
+++ src/pages/ProductsPage/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Header from "../../components/Header";
-import Layout from "../../components/Layout";
 import ToTopButton from "../../components/ToTopButton";
 import { FiltersProvider } from "../../context/FilterContext";
 import { ProductsProvider } from "../../context/ProductsContext";
@@ -10,13 +9,11 @@ import "./ProductsPage.css";
 const ProductsPage = () => {
   const [showFilter, setShowFilter] = useState(false);
   const toggleFilter = () => {
-    console.log(showFilter);
     setShowFilter(!showFilter);
   };
   return (
     <>
       <Header />
-
       <main className="container w-full row h-screen text-light">
         <ToTopButton />
         <ProductsProvider>
