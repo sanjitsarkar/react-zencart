feed340991ec67670cc917e9ca6a00e115c66a65
Sanjit <ssanjit856@gmail.com>
2022-04-05 00:18:12 +0530

    added loader

--- src/pages/HomePage/CategorySection.jsx
+++ src/pages/HomePage/CategorySection.jsx
@@ -9,8 +9,8 @@ const CategorySection = () => {
     <section className="section-category p-5  mr-0 pr-4 mt-0 pt-0">
       <h1 className="text-2xl mb-3 section-title">Shop By Category</h1>
       <div className="row gap-1 ">
-        {categories.loading && <h1>Loading...</h1>}
-        {!categories.loading && categories.data.length ? (
+        {!categories.loading &&
+          categories.data.length &&
           categories.data
             .slice(0, 6)
             .map((category) => (
@@ -19,10 +19,7 @@ const CategorySection = () => {
                 title={category.categoryName}
                 key={category._id}
               />
-            ))
-        ) : (
-          <h1>No Categories Found</h1>
-        )}
+            ))}
       </div>
     </section>
   );
