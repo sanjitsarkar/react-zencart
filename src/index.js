9167b32a341cf027dd70443d53481961b509b5dd
Sanjit <ssanjit856@gmail.com>
2022-04-05 13:38:04 +0530

    Fixed Provider Issue

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { makeServer } from "./server";
-<<<<<<< HEAD
 import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
-=======
 import { ProductsProvider } from "./context/ProductsContext";
 import { FiltersProvider } from "./context/FilterContext";
->>>>>>> pages/products
 
 // Call make Server
 makeServer();
@@ -18,19 +15,15 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <ToastProvider>
         <AuthProvider>
-          <App />
+          <ProductsProvider>
+            <FiltersProvider>
+              <App />
+            </FiltersProvider>
+          </ProductsProvider>
         </AuthProvider>
       </ToastProvider>
-=======
-      <ProductsProvider>
-        <FiltersProvider>
-          <App />
-        </FiltersProvider>
-      </ProductsProvider>
->>>>>>> pages/products
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
